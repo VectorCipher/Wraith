3ab250ccd1b22aa8ecb39f845740004e957f32a9
Naitik Pal <naitikpal513@gmail.com>
2026-06-25 14:12:00 +0530

    Fix the import issue of the module feed/sources

--- agent/feed/sources/__init__.py
+++ agent/feed/sources/__init__.py
@@ -1,1 +1,11 @@
 """WRAITH Feed Sources Package."""
+from .exploitdb import ExploitDBFetcher
+from .nuclei import NucleiFetcher
+from .nvd import NVDFetcher
+
+__all__ = [
+    'ExploitDBFetcher',
+    'NucleiFetcher',
+    'NVDFetcher',
+]
+    
